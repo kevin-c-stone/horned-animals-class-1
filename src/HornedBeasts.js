9c840de5f50c564dce61dbf1aa867706c261726e
Kevin C Stone <kevin.c.stone@outlook.com>
2021-10-26 15:43:57 -0700

    completed feature 1 and 2

--- src/HornedBeasts.js
+++ src/HornedBeasts.js
@@ -1,15 +1,29 @@
 import { Component } from "react";
 
 class HornedBeasts extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favorites: 0,
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ favorites: this.state.favorites + 1 });
+  };
+
   render() {
     return (
       <>
         <h2>{this.props.title}</h2>
         <img
           src={this.props.imageURL}
-          alt={"Narwhal baby"}
+          alt={this.props.keyword}
           title={this.props.title}
+          onClick={this.handleClick}
         ></img>
+        <p>&hearts;: {this.state.favorites}</p>
+        <p>Number of Horns: {this.props.horns}</p>
         <p>{this.props.description}</p>
       </>
     );
